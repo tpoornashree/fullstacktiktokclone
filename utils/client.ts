7dbe9caadda5475fe2d75cdff8301bd62555db48
tpoornashree <TPOORNASHREE@GMAIL.COM>
2022-10-01 20:59:21 +0530

    first commit

---
+++ utils/client.ts
@@ -0,0 +1,11 @@
+import sanityClient from '@sanity/client';
+
+export const client = sanityClient({
+  projectId: 'vaaszk41',
+  dataset: 'production',
+  apiVersion: '2022-03-10',
+  useCdn: false,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
+
+// 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
